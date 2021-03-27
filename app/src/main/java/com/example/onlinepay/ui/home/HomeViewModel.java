efac0413b95e9b85530eac370aa13228ae5fd72c
santosh kumar <santoshkumar26330@gmail.com>
2021-03-27 19:43:46 +0530

    lot of changes in this project

--- app/src/main/java/com/example/onlinepay/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/onlinepay/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("Balance");
     }
 
     public LiveData<String> getText() {
